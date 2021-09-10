4a1767e047d8ed79034d4a6eef251bc3baee9bcc
JereMitch <jere.mitch8@gmail.com>
2021-09-10 15:27:08 -0600

    annotated python file and changed Tuesday to Monday

--- databases/process.py
+++ databases/process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt")      #Connecting the python file to the text file
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file):    #Creating/Naming a function that reaches into the text file
+    for line in log_file:       #Creating a loop to go through the text file, giving a name to "line" to be used later
+        line = line.rstrip()    #Calling upon "line" as a name for the function ".rstrip" that makes the data more readable
+        day = line[0:3]         #Setting "day" equal to "line" at indexes 0 and 3
+        if day == "Mon":        #Conditional logic 
+            print(line)         #Print the day and the note attached to that day
 
 
-sales_reports(log_file)
+sales_reports(log_file)         #calling the function
